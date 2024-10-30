53bf067f5c2dbc33e38e65ec1008bb76d25b210b
Nana Leven <nana.leven@mail.de>
2024-10-30 15:32:13 +0100

    some light changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,19 @@ import { NavLink, Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <div>
-        <img src="" alt="" />
-        <h1>Delicously vegan</h1>
+      <div className="header-container">
+        <div>
+          <img src="" alt="" />
+          <h1>Delicously vegan</h1>
+        </div>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/recipes">Recipes</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+        <Link to="/">Login</Link>
       </div>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/recipes">Recipes</NavLink>
-        <NavLink to="/about">About</NavLink>
-      </nav>
-      <Link to="/">Login</Link>
+      <img src="" alt="" className="header-img" />
     </header>
   );
 }
